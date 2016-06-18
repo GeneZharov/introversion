6a9af566c102009098878a387a9e1c1f7a660186
Gene Zharov <gene.zharov@gmail.com>
2016-06-18 20:01:15 +0300

    New utilities for methods
    - D.m();
    - D.M();
    - D.m_();
    - D.mb.m();
    - D.mb.M();
    - D.mb.m_();

--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,6 @@ module.exports = function (config) {
     config.set({
         frameworks: ["jasmine"],
         browsers: ["Chrome"],
-        files: ["lib/*"]
+        files: ["lib/*.js"]
     });
 };
